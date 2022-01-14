4e3941266bda7cc75185f76bbbb3af847f2af62f
Alex de Sousa <alex@thebroken.link>
2022-01-14 08:49:03 +0100

    :sparkles: [API] Added function for getting the balance of an address

--- lib/icon.ex
+++ lib/icon.ex
@@ -1,5 +1,51 @@
 defmodule Icon do
   @moduledoc """
-  Documentation for `Icon`.
+  `Icon` is a library for interacting with the interoperable decentralized
+  aggregator network [ICON 2.0](https://icon.foundation).
   """
+  alias Icon.RPC.{Identity, Request}
+  alias Icon.Schema.{Error, Types.Address, Types.Loop}
+
+  @doc """
+  Gets the balance of an EOA or SCORE `address`. If the `address` is not provided,
+  it uses the one in the `identity`. The balance is returned in loop
+  (1 ICX = 10¹⁸ loop).
+
+  ## Examples
+
+  - Requesting the balance of the loaded identity:
+
+  ```elixir
+  iex> identity = Icon.RPC.Identity.new(private_key: "8ad9...")
+  iex> Icon.get_balance(identity)
+  {:ok, 2_045_995_000_000_000_000_000}
+  ```
+
+  - Requesting the balance of a wallet:
+
+  ```elixir
+  iex> identity = Icon.RPC.Identity.new()
+  iex> Icon.get_balance(identity, "hxbe258ceb872e08851f1f59694dac2558708ece11")
+  {:ok, 0}
+  ```
+  """
+  @spec get_balance(Identity.t()) :: {:ok, Loop.t()} | {:error, Error.t()}
+  @spec get_balance(Identity.t(), nil | Address.t()) ::
+          {:ok, Loop.t()}
+          | {:error, Error.t()}
+  def get_balance(identity, address \\ nil)
+
+  def get_balance(%Identity{} = identity, address) do
+    with {:ok, request} <- Request.Goloop.get_balance(identity, address),
+         {:ok, response} <- Request.send(request),
+         :error <- Loop.load(response) do
+      reason =
+        Error.new(
+          reason: :server_error,
+          message: "cannot cast balance to loop"
+        )
+
+      {:error, reason}
+    end
+  end
 end
---
+++ test/icon_test.exs
@@ -0,0 +1,155 @@
+defmodule IconTest do
+  use ExUnit.Case, async: true
+
+  alias Icon.RPC.Identity
+  alias Icon.Schema.Error
+
+  describe "get_balance/1" do
+    setup do
+      bypass = Bypass.open()
+
+      # Taken from Python ICON SDK tests.
+      private_key =
+        "8ad9889bcee734a2605a6c4c50dd8acd28f54e62b828b2c8991aa46bd32976bf"
+
+      identity =
+        Identity.new(
+          private_key: private_key,
+          node: "http://localhost:#{bypass.port}"
+        )
+
+      {:ok, bypass: bypass, identity: identity}
+    end
+
+    test "when the request is successful, returns own balance in loop", %{
+      identity: identity,
+      bypass: bypass
+    } do
+      Bypass.expect_once(bypass, "POST", "/api/v3", fn conn ->
+        response = result("0x2a")
+        Plug.Conn.resp(conn, 200, response)
+      end)
+
+      assert {:ok, 42} = Icon.get_balance(identity)
+    end
+
+    test "when the balance is not valid, returns error", %{
+      identity: identity,
+      bypass: bypass
+    } do
+      Bypass.expect_once(bypass, "POST", "/api/v3", fn conn ->
+        response = result("invalid")
+        Plug.Conn.resp(conn, 200, response)
+      end)
+
+      assert {:error,
+              %Error{
+                reason: :server_error,
+                message: "cannot cast balance to loop"
+              }} = Icon.get_balance(identity)
+    end
+
+    test "when server responds with an error, errors", %{
+      identity: identity,
+      bypass: bypass
+    } do
+      Bypass.expect_once(bypass, "POST", "/api/v3", fn conn ->
+        error =
+          error(%{
+            code: -31_000,
+            message: "System error"
+          })
+
+        Plug.Conn.resp(conn, 400, error)
+      end)
+
+      assert {:error,
+              %Error{
+                reason: :system_error,
+                message: "System error"
+              }} = Icon.get_balance(identity)
+    end
+  end
+
+  describe "get_balance/2" do
+    setup do
+      bypass = Bypass.open()
+      identity = Identity.new(node: "http://localhost:#{bypass.port}")
+      wallet = "hxfd7e4560ba363f5aabd32caac7317feeee70ea57"
+
+      {:ok, bypass: bypass, identity: identity, wallet: wallet}
+    end
+
+    test "when the request is successful, returns own balance in loop", %{
+      wallet: wallet,
+      identity: identity,
+      bypass: bypass
+    } do
+      Bypass.expect_once(bypass, "POST", "/api/v3", fn conn ->
+        response = result("0x2a")
+        Plug.Conn.resp(conn, 200, response)
+      end)
+
+      assert {:ok, 42} = Icon.get_balance(identity, wallet)
+    end
+
+    test "when the balance is not valid, returns error", %{
+      wallet: wallet,
+      identity: identity,
+      bypass: bypass
+    } do
+      Bypass.expect_once(bypass, "POST", "/api/v3", fn conn ->
+        response = result("invalid")
+        Plug.Conn.resp(conn, 200, response)
+      end)
+
+      assert {:error,
+              %Error{
+                reason: :server_error,
+                message: "cannot cast balance to loop"
+              }} = Icon.get_balance(identity, wallet)
+    end
+
+    test "when server responds with an error, errors", %{
+      wallet: wallet,
+      identity: identity,
+      bypass: bypass
+    } do
+      Bypass.expect_once(bypass, "POST", "/api/v3", fn conn ->
+        error =
+          error(%{
+            code: -31_000,
+            message: "System error"
+          })
+
+        Plug.Conn.resp(conn, 400, error)
+      end)
+
+      assert {:error,
+              %Error{
+                reason: :system_error,
+                message: "System error"
+              }} = Icon.get_balance(identity, wallet)
+    end
+  end
+
+  @spec result(any()) :: binary()
+  defp result(result) do
+    %{
+      "jsonrpc" => "2.0",
+      "id" => :erlang.system_time(:microsecond),
+      "result" => result
+    }
+    |> Jason.encode!()
+  end
+
+  @spec error(map()) :: binary()
+  defp error(error) when is_map(error) do
+    %{
+      "jsonrpc" => "2.0",
+      "id" => :erlang.system_time(:microsecond),
+      "error" => error
+    }
+    |> Jason.encode!()
+  end
+end
