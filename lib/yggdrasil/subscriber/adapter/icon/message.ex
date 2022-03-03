516e0ff87dfe56d37c6ab9f9053342dd5f29664e
Alex de Sousa <alex@thebroken.link>
2022-03-03 21:37:59 +0100

    :sparkles: [WebSocket] Added ICON 2.0 websocket message decoder

--- lib/yggdrasil/subscriber/adapter/icon/message.ex
+++ lib/yggdrasil/subscriber/adapter/icon/message.ex
@@ -1,9 +1,35 @@
 defmodule Yggdrasil.Subscriber.Adapter.Icon.Message do
   @moduledoc """
   This module defines functions to deal with ICON 2.0 websocket messages.
+
+  When a connection is established with the ICON 2.0 websocket, we need to send
+  a `text` frame with a JSON payload to state our intention. There are two
+  channels:
+
+  - `:block` for receiving both ticks on every block and event logs specific to
+    the events we're filtering.
+  - `:event` for receiving updates for a specific event log.
   """
+  alias Icon.RPC.Identity
+  alias Icon.Schema
   alias Icon.Schema.Type
   alias Yggdrasil.Channel
+
+  @doc """
+  Decodes an incoming message from the ICON 2.0 websocket.
+  """
+  @spec decode(Channel.t(), map()) ::
+          {:ok, [Schema.Types.EventLog.t()]}
+          | {:error, Schema.Error.t()}
+  def decode(channel, notification)
+
+  def decode(%Channel{name: %{source: :block}} = channel, notification) do
+    decode_block(channel, notification)
+  end
+
+  def decode(%Channel{name: %{source: :event}} = channel, notification) do
+    decode_event(channel, notification)
+  end
 
   @doc """
   Encodes a request for the ICON 2.0 websocket. It receives the `height` and
@@ -17,6 +43,205 @@ defmodule Yggdrasil.Subscriber.Adapter.Icon.Message do
       |> Jason.encode!()
 
     {:text, data}
+  end
+
+  ##################
+  # Decoding helpers
+
+  @spec decode_block(Channel.t(), map()) ::
+          {:ok, [Schema.Types.EventLog.t() | Schema.Types.Block.Tick.t()]}
+          | {:error, Schema.Error.t()}
+  defp decode_block(channel, notification)
+
+  defp decode_block(
+         %Channel{name: info} = channel,
+         %{
+           "height" => height,
+           "hash" => hash,
+           "indexes" => indexes,
+           "events" => events
+         }
+       ) do
+    identity = info[:identity] || Identity.new()
+    base = %{"height" => height, "hash" => hash}
+    {:ok, [tick]} = decode_block(channel, base)
+
+    indexes
+    |> Stream.zip(events)
+    |> Stream.map(fn {indexes, events} -> Enum.zip(indexes, events) end)
+    |> Stream.flat_map(& &1)
+    |> Stream.map(fn {index, events} ->
+      %{"index" => index, "events" => events}
+    end)
+    |> Enum.map(&Map.merge(base, &1))
+    |> decode_events(identity)
+    |> case do
+      {:ok, event_logs} ->
+        {:ok, [tick | event_logs]}
+
+      {:error, _} = error ->
+        error
+    end
+  end
+
+  defp decode_block(%Channel{}, notification) do
+    Schema.Types.Block.Tick
+    |> Schema.generate()
+    |> Schema.new(notification)
+    |> Schema.load()
+    |> Schema.apply(into: Schema.Types.Block.Tick)
+    |> case do
+      {:ok, %Schema.Types.Block.Tick{} = tick} ->
+        {:ok, [tick]}
+
+      {:error, _} = error ->
+        error
+    end
+  end
+
+  @spec decode_events(
+          [map()],
+          Identity.t(),
+          nil | Schema.Types.Block.t(),
+          [Schema.Types.EventLog.t()]
+        ) ::
+          {:ok, [Schema.Types.EventLog.t()]}
+          | {:error, Schema.Error.t()}
+  defp decode_events(notifications, identity, block \\ nil, event_logs \\ [])
+
+  defp decode_events([], _identity, _block, event_logs) do
+    {:ok, event_logs}
+  end
+
+  defp decode_events([first | _] = notifications, identity, nil, event_logs) do
+    with {:ok, height} <- decode_block_height(first),
+         {:ok, block} <- Icon.get_block(identity, height - 1),
+         {:ok, event_logs} <-
+           decode_events(notifications, identity, block, event_logs) do
+      {:ok, event_logs}
+    else
+      :error ->
+        reason = "cannot decode information in notification"
+        {:error, Schema.Error.new(code: -32_000, message: reason)}
+
+      {:error, _} = error ->
+        error
+    end
+  end
+
+  defp decode_events([notification | notifications], identity, block, events) do
+    with {:ok, tx_index} <- decode_transaction_index(notification),
+         {:ok, events_indexes} <- decode_events_indexes(notification),
+         {:ok, transaction} <- get_transaction(block, tx_index),
+         {:ok, new_events} <- get_event_logs(identity, transaction) do
+      new_events = filter_logs(new_events, events_indexes)
+      decode_events(notifications, identity, block, new_events ++ events)
+    end
+  end
+
+  @spec decode_event(Channel.t(), map()) ::
+          {:ok, [Schema.Types.EventLog.t()]}
+          | {:error, Schema.Error.t()}
+  defp decode_event(channel, notification)
+
+  defp decode_event(%Channel{name: info}, notification) do
+    identity = info[:identity] || Identity.new()
+
+    with {:ok, tx_index} <- decode_transaction_index(notification),
+         {:ok, events_indexes} <- decode_events_indexes(notification),
+         {:ok, height} <- decode_block_height(notification),
+         {:ok, block} <- Icon.get_block(identity, height - 1),
+         {:ok, tx} <- get_transaction(block, tx_index),
+         {:ok, event_logs} <- get_event_logs(identity, tx) do
+      {:ok, filter_logs(event_logs, events_indexes)}
+    else
+      :error ->
+        reason = "cannot decode information in notification"
+        {:error, Schema.Error.new(code: -32_000, message: reason)}
+
+      {:error, _} = error ->
+        error
+    end
+  end
+
+  @spec decode_transaction_index(map()) ::
+          {:ok, Schema.Types.Integer.t()}
+          | :error
+  defp decode_transaction_index(notification)
+
+  defp decode_transaction_index(%{"index" => encoded}) do
+    Schema.Types.Integer.load(encoded)
+  end
+
+  defp decode_transaction_index(_), do: :error
+
+  @spec decode_events_indexes(map()) ::
+          {:ok, [Schema.Types.Integer.t()]}
+          | :error
+  defp decode_events_indexes(notification)
+
+  defp decode_events_indexes(%{"events" => events}) do
+    Enum.reduce_while(events, {:ok, []}, fn event_index, {:ok, indexes} ->
+      case Schema.Types.Integer.load(event_index) do
+        {:ok, index} ->
+          {:cont, {:ok, [index | indexes]}}
+
+        :error ->
+          {:halt, :error}
+      end
+    end)
+  end
+
+  defp decode_events_indexes(_), do: :error
+
+  @spec decode_block_height(map()) :: {:ok, Schema.Types.Integer.t()} | :error
+  defp decode_block_height(notification)
+
+  defp decode_block_height(%{"height" => height}) do
+    Schema.Types.Integer.load(height)
+  end
+
+  defp decode_block_height(_), do: :error
+
+  @spec get_transaction(Schema.Types.Block.t(), non_neg_integer()) ::
+          {:ok, Schema.Types.Transaction.t()}
+          | {:error, Schema.Error.t()}
+  defp get_transaction(block, index)
+
+  defp get_transaction(%Schema.Types.Block{} = block, index) do
+    case Enum.at(block.confirmed_transaction_list, index) do
+      nil ->
+        reason =
+          "cannot find the transaction index #{index} on block with height #{block.height}"
+
+        {:error, Schema.Error.new(code: -32_000, message: reason)}
+
+      transaction ->
+        {:ok, transaction}
+    end
+  end
+
+  @spec get_event_logs(Identity.t(), Schema.Types.Transaction.t()) ::
+          {:ok, [Schema.Types.EventLog.t()]}
+          | {:error, Schema.Error.t()}
+  defp get_event_logs(identity, transaction)
+
+  defp get_event_logs(identity, %Schema.Types.Transaction{} = transaction) do
+    with {:ok, %Schema.Types.Transaction.Result{} = result} <-
+           Icon.get_transaction_result(identity, transaction.txHash) do
+      {:ok, result.eventLogs}
+    end
+  end
+
+  @spec filter_logs([Schema.Types.EventLog.t()], [non_neg_integer()]) ::
+          [Schema.Types.EventLog.t()]
+  defp filter_logs(event_logs, indexes)
+
+  defp filter_logs(event_logs, indexes) do
+    0..(length(event_logs) - 1)
+    |> Stream.zip(event_logs)
+    |> Stream.filter(fn {x, _event} -> x in indexes end)
+    |> Enum.map(fn {_, event_log} -> event_log end)
   end
 
   ##################
--- test/yggdrasil/subscriber/adapter/icon/message_test.exs
+++ test/yggdrasil/subscriber/adapter/icon/message_test.exs
@@ -1,10 +1,626 @@
 defmodule Yggdrasil.Subscriber.Adapter.Icon.MessageTest do
   use ExUnit.Case, async: true
 
+  alias Icon.RPC.Identity
+  alias Icon.Schema.Error
+  alias Icon.Schema.Types.Block.Tick
+  alias Icon.Schema.Types.EventLog
   alias Yggdrasil.Channel
   alias Yggdrasil.Subscriber.Adapter.Icon.Message
 
-  describe "encoding/2 for block" do
+  describe "decode/2 for block" do
+    setup do
+      bypass = Bypass.open()
+
+      channel = %Channel{
+        name: %{
+          source: :block,
+          data: [
+            %{
+              addr: "cxb0776ee37f5b45bfaea8cff1d8232fbb6122ec32",
+              event: "Transfer(Address,Address,int)"
+            }
+          ],
+          identity: Identity.new(node: "http://localhost:#{bypass.port}")
+        }
+      }
+
+      {:ok, bypass: bypass, channel: channel}
+    end
+
+    test "filters event logs correctly", %{
+      bypass: bypass,
+      channel: channel
+    } do
+      # Target data
+      block_height = "0x29"
+
+      tx_hash =
+        "0xf8773bc17c4b84753a8dbb7bcf663c5a7b90d84770949d2966857fe1106ee5e9"
+
+      target_transaction = %{"txHash" => tx_hash}
+
+      target_event_log = %{
+        "scoreAddress" => Enum.at(channel.name.data, 0).addr,
+        "indexed" => [
+          Enum.at(channel.name.data, 0).event,
+          "hxfd7e4560ba363f5aabd32caac7317feeee70ea57",
+          "hx2e243ad926ac48d15156756fce28314357d49d83"
+        ],
+        "data" => [
+          "0x2a"
+        ]
+      }
+
+      # Server mock
+      block = %{
+        "block_hash" =>
+          "0x4ec23aec0d1834981ac6793537b9358dfa0d9ce5b4237a550b62e1b63bb84ee6",
+        "height" => block_height,
+        "confirmed_transaction_list" => [
+          %{
+            "txHash" =>
+              "0xf8773bc17c4b84753a8dbb7bcf663c5a7b90d84770949d2966857fe1106ee5e9"
+          },
+          target_transaction
+        ]
+      }
+
+      transaction = %{
+        "txHash" => tx_hash,
+        "eventLogs" => [
+          %{
+            "scoreAddress" => "cx2609b924e33ef00b648a409245c7ea394c467824",
+            "indexed" => ["Event()"],
+            "data" => []
+          },
+          target_event_log
+        ]
+      }
+
+      Bypass.expect(bypass, "POST", "/api/v3", fn conn ->
+        {:ok, payload, conn} = Plug.Conn.read_body(conn)
+
+        result =
+          case Jason.decode(payload) do
+            {:ok, %{"method" => "icx_getBlockByHeight"}} ->
+              result(block)
+
+            {:ok, %{"method" => "icx_getTransactionResult"}} ->
+              result(transaction)
+          end
+
+        Plug.Conn.resp(conn, 200, result)
+      end)
+
+      # Actual test
+      notification = %{
+        "height" => "0x2a",
+        "hash" =>
+          "0xc71303ef8543d04b5dc1ba6579132b143087c68db1b2168786408fcbce568238",
+        "indexes" => [
+          [
+            "0x1"
+          ]
+        ],
+        "events" => [
+          [
+            ["0x1"]
+          ]
+        ]
+      }
+
+      assert {
+               :ok,
+               [
+                 %Tick{
+                   hash:
+                     "0xc71303ef8543d04b5dc1ba6579132b143087c68db1b2168786408fcbce568238",
+                   height: 42
+                 },
+                 %EventLog{
+                   header: "Transfer(Address,Address,int)",
+                   indexed: [
+                     "hxfd7e4560ba363f5aabd32caac7317feeee70ea57",
+                     "hx2e243ad926ac48d15156756fce28314357d49d83"
+                   ],
+                   data: [
+                     42
+                   ]
+                 }
+               ]
+             } = Message.decode(channel, notification)
+    end
+
+    test "errors when tick cannot be decoded", %{
+      channel: channel
+    } do
+      notification = %{
+        "height" => "invalid",
+        "hash" =>
+          "0xc71303ef8543d04b5dc1ba6579132b143087c68db1b2168786408fcbce568238"
+      }
+
+      assert {
+               :error,
+               %Error{
+                 code: -32_602,
+                 reason: :invalid_params,
+                 message: "height is invalid"
+               }
+             } = Message.decode(channel, notification)
+    end
+
+    test "errors when it cannot decode transaction index", %{
+      bypass: bypass,
+      channel: channel
+    } do
+      block_height = "0x29"
+
+      block = %{
+        "block_hash" =>
+          "0x4ec23aec0d1834981ac6793537b9358dfa0d9ce5b4237a550b62e1b63bb84ee6",
+        "height" => block_height,
+        "confirmed_transaction_list" => []
+      }
+
+      Bypass.expect(bypass, "POST", "/api/v3", fn conn ->
+        result = result(block)
+        Plug.Conn.resp(conn, 200, result)
+      end)
+
+      notification = %{
+        "height" => "0x2a",
+        "hash" =>
+          "0xc71303ef8543d04b5dc1ba6579132b143087c68db1b2168786408fcbce568238",
+        "indexes" => [["invalid"]],
+        "events" => [[["0x1"]]]
+      }
+
+      assert {
+               :error,
+               %Error{
+                 code: -32_000,
+                 reason: :server_error,
+                 message: "cannot decode information in notification"
+               }
+             } = Message.decode(channel, notification)
+    end
+
+    test "errors when it cannot get block", %{
+      bypass: bypass,
+      channel: channel
+    } do
+      Bypass.expect(bypass, "POST", "/api/v3", fn conn ->
+        Plug.Conn.resp(conn, 500, "")
+      end)
+
+      notification = %{
+        "height" => "0x2a",
+        "hash" =>
+          "0xc71303ef8543d04b5dc1ba6579132b143087c68db1b2168786408fcbce568238",
+        "indexes" => [["0x1"]],
+        "events" => [[["0x1"]]]
+      }
+
+      assert {
+               :error,
+               %Error{
+                 code: -31_000,
+                 reason: :system_error,
+                 message: "System error"
+               }
+             } = Message.decode(channel, notification)
+    end
+
+    test "errors when it cannot find transaction in block", %{
+      bypass: bypass,
+      channel: channel
+    } do
+      block_height = "0x29"
+
+      block = %{
+        "block_hash" =>
+          "0x4ec23aec0d1834981ac6793537b9358dfa0d9ce5b4237a550b62e1b63bb84ee6",
+        "height" => block_height,
+        "confirmed_transaction_list" => []
+      }
+
+      Bypass.expect(bypass, "POST", "/api/v3", fn conn ->
+        result = result(block)
+        Plug.Conn.resp(conn, 200, result)
+      end)
+
+      notification = %{
+        "height" => "0x2a",
+        "hash" =>
+          "0xc71303ef8543d04b5dc1ba6579132b143087c68db1b2168786408fcbce568238",
+        "indexes" => [["0x1"]],
+        "events" => [[["0x1"]]]
+      }
+
+      assert {
+               :error,
+               %Error{
+                 code: -32_000,
+                 reason: :server_error,
+                 message:
+                   "cannot find the transaction index 1 on block with height 41"
+               }
+             } = Message.decode(channel, notification)
+    end
+  end
+
+  describe "decode/2 for event" do
+    setup do
+      bypass = Bypass.open()
+
+      channel = %Channel{
+        name: %{
+          source: :event,
+          data: %{
+            addr: "cxb0776ee37f5b45bfaea8cff1d8232fbb6122ec32",
+            event: "Transfer(Address,Address,int)"
+          },
+          identity: Identity.new(node: "http://localhost:#{bypass.port}")
+        }
+      }
+
+      {:ok, bypass: bypass, channel: channel}
+    end
+
+    test "filters event logs correctly", %{
+      bypass: bypass,
+      channel: channel
+    } do
+      # Target data
+      block_height = "0x29"
+
+      tx_hash =
+        "0xf8773bc17c4b84753a8dbb7bcf663c5a7b90d84770949d2966857fe1106ee5e9"
+
+      target_transaction = %{"txHash" => tx_hash}
+
+      target_event_log = %{
+        "scoreAddress" => channel.name.data.addr,
+        "indexed" => [
+          channel.name.data.event,
+          "hxfd7e4560ba363f5aabd32caac7317feeee70ea57",
+          "hx2e243ad926ac48d15156756fce28314357d49d83"
+        ],
+        "data" => [
+          "0x2a"
+        ]
+      }
+
+      # Server mock
+      block = %{
+        "block_hash" =>
+          "0x4ec23aec0d1834981ac6793537b9358dfa0d9ce5b4237a550b62e1b63bb84ee6",
+        "height" => block_height,
+        "confirmed_transaction_list" => [
+          %{
+            "txHash" =>
+              "0xf8773bc17c4b84753a8dbb7bcf663c5a7b90d84770949d2966857fe1106ee5e9"
+          },
+          target_transaction
+        ]
+      }
+
+      transaction = %{
+        "txHash" => tx_hash,
+        "eventLogs" => [
+          %{
+            "scoreAddress" => "cx2609b924e33ef00b648a409245c7ea394c467824",
+            "indexed" => ["Event()"],
+            "data" => []
+          },
+          target_event_log
+        ]
+      }
+
+      Bypass.expect(bypass, "POST", "/api/v3", fn conn ->
+        {:ok, payload, conn} = Plug.Conn.read_body(conn)
+
+        result =
+          case Jason.decode(payload) do
+            {:ok, %{"method" => "icx_getBlockByHeight"}} ->
+              result(block)
+
+            {:ok, %{"method" => "icx_getTransactionResult"}} ->
+              result(transaction)
+          end
+
+        Plug.Conn.resp(conn, 200, result)
+      end)
+
+      # Actual test
+      notification = %{
+        "height" => "0x2a",
+        "hash" =>
+          "0xc71303ef8543d04b5dc1ba6579132b143087c68db1b2168786408fcbce568238",
+        "index" => "0x1",
+        "events" => [
+          "0x1"
+        ]
+      }
+
+      assert {
+               :ok,
+               [
+                 %EventLog{
+                   header: "Transfer(Address,Address,int)",
+                   indexed: [
+                     "hxfd7e4560ba363f5aabd32caac7317feeee70ea57",
+                     "hx2e243ad926ac48d15156756fce28314357d49d83"
+                   ],
+                   data: [
+                     42
+                   ]
+                 }
+               ]
+             } = Message.decode(channel, notification)
+    end
+
+    test "errors when it cannot decode index", %{
+      channel: channel
+    } do
+      notification = %{
+        "height" => "0x2a",
+        "hash" =>
+          "0xc71303ef8543d04b5dc1ba6579132b143087c68db1b2168786408fcbce568238",
+        "index" => "invalid",
+        "events" => [
+          "0x1"
+        ]
+      }
+
+      assert {
+               :error,
+               %Error{
+                 code: -32_000,
+                 reason: :server_error,
+                 message: "cannot decode information in notification"
+               }
+             } = Message.decode(channel, notification)
+    end
+
+    test "errors when there's no transaction index", %{
+      channel: channel
+    } do
+      notification = %{
+        "height" => "0x2a",
+        "hash" =>
+          "0xc71303ef8543d04b5dc1ba6579132b143087c68db1b2168786408fcbce568238",
+        "events" => [
+          "0x1"
+        ]
+      }
+
+      assert {
+               :error,
+               %Error{
+                 code: -32_000,
+                 reason: :server_error,
+                 message: "cannot decode information in notification"
+               }
+             } = Message.decode(channel, notification)
+    end
+
+    test "errors when it cannot decode events", %{
+      channel: channel
+    } do
+      notification = %{
+        "height" => "0x2a",
+        "hash" =>
+          "0xc71303ef8543d04b5dc1ba6579132b143087c68db1b2168786408fcbce568238",
+        "index" => "0x1",
+        "events" => [
+          "invalid"
+        ]
+      }
+
+      assert {
+               :error,
+               %Error{
+                 code: -32_000,
+                 reason: :server_error,
+                 message: "cannot decode information in notification"
+               }
+             } = Message.decode(channel, notification)
+    end
+
+    test "errors when there are no events", %{
+      channel: channel
+    } do
+      notification = %{
+        "height" => "0x2a",
+        "hash" =>
+          "0xc71303ef8543d04b5dc1ba6579132b143087c68db1b2168786408fcbce568238",
+        "index" => "0x1"
+      }
+
+      assert {
+               :error,
+               %Error{
+                 code: -32_000,
+                 reason: :server_error,
+                 message: "cannot decode information in notification"
+               }
+             } = Message.decode(channel, notification)
+    end
+
+    test "errors when it cannot decode height", %{
+      channel: channel
+    } do
+      notification = %{
+        "height" => "invalid",
+        "hash" =>
+          "0xc71303ef8543d04b5dc1ba6579132b143087c68db1b2168786408fcbce568238",
+        "index" => "0x1",
+        "events" => [
+          "0x1"
+        ]
+      }
+
+      assert {
+               :error,
+               %Error{
+                 code: -32_000,
+                 reason: :server_error,
+                 message: "cannot decode information in notification"
+               }
+             } = Message.decode(channel, notification)
+    end
+
+    test "errors when there's no height", %{
+      channel: channel
+    } do
+      notification = %{
+        "hash" =>
+          "0xc71303ef8543d04b5dc1ba6579132b143087c68db1b2168786408fcbce568238",
+        "index" => "0x1",
+        "events" => [
+          "0x1"
+        ]
+      }
+
+      assert {
+               :error,
+               %Error{
+                 code: -32_000,
+                 reason: :server_error,
+                 message: "cannot decode information in notification"
+               }
+             } = Message.decode(channel, notification)
+    end
+
+    test "errors when it cannot get block", %{
+      bypass: bypass,
+      channel: channel
+    } do
+      Bypass.expect(bypass, "POST", "/api/v3", fn conn ->
+        Plug.Conn.resp(conn, 500, "")
+      end)
+
+      notification = %{
+        "height" => "0x2a",
+        "hash" =>
+          "0xc71303ef8543d04b5dc1ba6579132b143087c68db1b2168786408fcbce568238",
+        "index" => "0x1",
+        "events" => [
+          "0x1"
+        ]
+      }
+
+      assert {
+               :error,
+               %Error{
+                 code: -31_000,
+                 reason: :system_error,
+                 message: "System error"
+               }
+             } = Message.decode(channel, notification)
+    end
+
+    test "errors when it cannot find transaction in block", %{
+      bypass: bypass,
+      channel: channel
+    } do
+      block_height = "0x29"
+
+      block = %{
+        "block_hash" =>
+          "0x4ec23aec0d1834981ac6793537b9358dfa0d9ce5b4237a550b62e1b63bb84ee6",
+        "height" => block_height,
+        "confirmed_transaction_list" => []
+      }
+
+      Bypass.expect(bypass, "POST", "/api/v3", fn conn ->
+        result = result(block)
+        Plug.Conn.resp(conn, 200, result)
+      end)
+
+      notification = %{
+        "height" => "0x2a",
+        "hash" =>
+          "0xc71303ef8543d04b5dc1ba6579132b143087c68db1b2168786408fcbce568238",
+        "index" => "0x1",
+        "events" => [
+          "0x1"
+        ]
+      }
+
+      assert {
+               :error,
+               %Error{
+                 code: -32_000,
+                 reason: :server_error,
+                 message:
+                   "cannot find the transaction index 1 on block with height 41"
+               }
+             } = Message.decode(channel, notification)
+    end
+
+    test "errors when it cannot find the transaction", %{
+      bypass: bypass,
+      channel: channel
+    } do
+      block_height = "0x29"
+
+      tx_hash =
+        "0xf8773bc17c4b84753a8dbb7bcf663c5a7b90d84770949d2966857fe1106ee5e9"
+
+      target_transaction = %{"txHash" => tx_hash}
+
+      # Server mock
+      block = %{
+        "block_hash" =>
+          "0x4ec23aec0d1834981ac6793537b9358dfa0d9ce5b4237a550b62e1b63bb84ee6",
+        "height" => block_height,
+        "confirmed_transaction_list" => [
+          %{
+            "txHash" =>
+              "0xf8773bc17c4b84753a8dbb7bcf663c5a7b90d84770949d2966857fe1106ee5e9"
+          },
+          target_transaction
+        ]
+      }
+
+      Bypass.expect(bypass, "POST", "/api/v3", fn conn ->
+        {:ok, payload, conn} = Plug.Conn.read_body(conn)
+
+        case Jason.decode(payload) do
+          {:ok, %{"method" => "icx_getBlockByHeight"}} ->
+            Plug.Conn.resp(conn, 200, result(block))
+
+          {:ok, %{"method" => "icx_getTransactionResult"}} ->
+            Plug.Conn.resp(conn, 500, "")
+        end
+      end)
+
+      notification = %{
+        "height" => "0x2a",
+        "hash" =>
+          "0xc71303ef8543d04b5dc1ba6579132b143087c68db1b2168786408fcbce568238",
+        "index" => "0x1",
+        "events" => [
+          "0x1"
+        ]
+      }
+
+      assert {
+               :error,
+               %Error{
+                 code: -31_000,
+                 reason: :system_error,
+                 message: "System error"
+               }
+             } = Message.decode(channel, notification)
+    end
+  end
+
+  describe "encode/2 for block" do
     test "sets height" do
       height = 42
       channel = %Channel{name: %{source: :block}}
@@ -76,7 +692,7 @@ defmodule Yggdrasil.Subscriber.Adapter.Icon.MessageTest do
     end
   end
 
-  describe "encoding/2 for event" do
+  describe "encode/2 for event" do
     test "sets height" do
       height = 42
       channel = %Channel{name: %{source: :event, data: %{event: "Event()"}}}
@@ -326,5 +942,15 @@ defmodule Yggdrasil.Subscriber.Adapter.Icon.MessageTest do
       assert {:text, data} = Message.encode(height, channel)
       assert %{"data" => [nil]} = Jason.decode!(data)
     end
+  end
+
+  @spec result(any()) :: binary()
+  defp result(result) do
+    %{
+      "jsonrpc" => "2.0",
+      "id" => :erlang.system_time(:microsecond),
+      "result" => result
+    }
+    |> Jason.encode!()
   end
 end
